08fcdf181fa8eafd4fa8cf365c28af402847dcc5
Alan Resende <alan.resende@live.com>
2023-10-21 00:36:01 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -0,0 +1,39 @@
+package br.com.igorbag.githubsearch.ui.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.igorbag.githubsearch.R
+import br.com.igorbag.githubsearch.domain.Repository
+import br.com.igorbag.githubsearch.ui.viewHolder.ViewHolder
+
+class RepositoryAdapter(private val repositories: List<Repository>) :
+    RecyclerView.Adapter<ViewHolder>() {
+
+    var repositoryItemLister: (Repository) -> Unit = {}
+    var btnShareLister: (Repository) -> Unit = {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nome.text = repositories[position].name
+        holder.cardView.setOnClickListener {
+            repositoryItemLister(repositories[position])
+            Log.e("Opa", "Opa")
+        }
+
+        holder.imgFavorite.setOnClickListener {
+            btnShareLister(repositories[position])
+            Log.e("Favorite", "Favorite")
+        }
+    }
+    override fun getItemCount(): Int =repositories.size
+
+}
+
+
